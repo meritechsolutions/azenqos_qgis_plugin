d31ed62ca5eb8cb8f7c7960dc15bd0c502b60167
Vuttichai <vuttichai.lor@outlook.com>
2021-02-19 11:06:17 +0700

    fix 5g params not show

--- Azenqos/test_nr_radio_params.py
+++ Azenqos/test_nr_radio_params.py
@@ -15,8 +15,8 @@ def test():
     with sqlite3.connect(dbfp) as dbcon:
         df = nr_query.get_nr_radio_params_disp_df(dbcon, "2020-08-18 13:47:42.382")
         print("df.head():\n %s" % df.head(20))
-        assert df.iloc[1, 1] == 41
-        assert len(df) == 10
+        assert df.iloc[1, 1] == 1
+        assert len(df) == 16
         assert len(df.columns) == 9
 
 

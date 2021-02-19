f3a69b5093395be21525b792e2548376c8ea96e8
Vuttichai <vuttichai.lor@outlook.com>
2021-02-19 17:09:28 +0700

    fix nr radio params

--- Azenqos/test_nr_radio_params.py
+++ Azenqos/test_nr_radio_params.py
@@ -13,9 +13,9 @@ def test():
     dbfp = integration_test_helpers.unzip_azm_to_tmp_get_dbfp(azmfp)
 
     with sqlite3.connect(dbfp) as dbcon:
-        df = nr_query.get_nr_radio_params_disp_df(dbcon, "2020-08-18 13:47:42.382")
+        df = nr_query.get_nr_radio_params_disp_df(dbcon, "2020-08-18 13:48:02.356")
         print("df.head():\n %s" % df.head(20))
-        assert df.iloc[1, 1] == 1
+        assert df.iloc[2, 1] == 41
         assert len(df) == 16
         assert len(df.columns) == 9
 

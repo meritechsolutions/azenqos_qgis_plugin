251a7e1d85334eff11b03800bab97de48a0a5d40
Vuttichai <vuttichai.lor@outlook.com>
2021-02-22 14:19:04 +0700

    fix mos sound

--- Azenqos/signalling_query.py
+++ Azenqos/signalling_query.py
@@ -350,8 +350,8 @@ def get_signalling(dbcon, time_before):
 
 def get_events(dbcon, time_before):
     sqls = [
-        "select log_hash, time, name, info, '' as wave_file from events",
-        "select log_hash, time, 'MOS Score' || ' ' ||  polqa_mos as name, polqa_output_text as info, wav_filename as wave_file from polqa_mos"
+        "select log_hash, time, name, info, '' as output_text, '' as wave_file from events",
+        "select log_hash, time, 'MOS Score' as name, polqa_mos as info, polqa_output_text as output_text, wav_filename as wave_file from polqa_mos"
     ]
     df_list = []
     for sql in sqls:

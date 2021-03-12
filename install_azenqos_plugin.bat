9d431a991ef81fbd7168e8fedd596f263bca3d4f
Vuttichai <vuttichai.lor@outlook.com>
2021-03-12 11:33:44 +0700

    Update plugin installer for QGIS3.16

--- install_azenqos_plugin.bat
+++ install_azenqos_plugin.bat
@@ -6,15 +6,35 @@ echo - Any existing Azenqos plugins would get deleted and replaced
 echo == Finding local QGIS installation folder...
 For /F "Skip=1 Tokens=2*" %%A In (
     'reg query "HKLM\SOFTWARE\QGIS 3.10" -v InstallPath 2^>Nul'
-) Do Set "EP=%%~B"
-Echo Found QGIS InstallPath: %EP%
+) Do Set "EP10=%%~B"
 
-echo == Preparing QGIS cmd env...
-call "%EP%\bin\o4w_env.bat" || (echo INSTALL FAILED && pause && exit 1)
-call "py3_env" || (echo INSTALL FAILED && pause && exit 1)
+if defined EP10 (
+    Echo Found QGIS InstallPath: %EP10%
+    echo == Preparing QGIS10 cmd env...
+    call "%EP10%\bin\o4w_env.bat" || (echo INSTALL FAILED && pause && exit 1)
+    call "py3_env" || (echo INSTALL FAILED && pause && exit 1)
+    echo == Installing required python packages into QGIS's python env...
+    python3 -m pip install -r requirements.txt || (echo INSTALL FAILED && pause && exit 1)
+)
 
-echo == Installing required python packages into QGIS's python env...
-python3 -m pip install -r requirements.txt || (echo INSTALL FAILED && pause && exit 1)
+For /F "Skip=1 Tokens=2*" %%A In (
+    'reg query "HKLM\SOFTWARE\QGIS 3.16" -v InstallPath 2^>Nul'
+) Do Set "EP16=%%~B"
+
+if defined EP16 (
+    Echo Found QGIS InstallPath: %EP16%
+    echo == Preparing QGIS16 cmd env...
+    call "%EP16%\bin\o4w_env.bat" || (echo INSTALL FAILED && pause && exit 1)
+    call "py3_env" || (echo INSTALL FAILED && pause && exit 1)
+    echo == Installing required python packages into QGIS's python env...
+    python3 -m pip install -r requirements.txt || (echo INSTALL FAILED && pause && exit 1)
+)
+
+if not defined EP16 (
+    if not defined EP10 (
+        echo NO QGIS FOUND && pause && exit 1
+    )
+) 
 
 echo == Removing any existing plugin folders...
 rmdir /q /s "%appdata%\QGIS\QGIS3\profiles\default\python\plugins\Azenqos"

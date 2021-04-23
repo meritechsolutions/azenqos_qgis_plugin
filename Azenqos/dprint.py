b2ce60a0f0db4f5de853512775df3a7dc9cae8d7
Kasidit Yusuf <ykasidit@gmail.com>
2021-04-23 14:40:59 +0700

    linux linechare3 lowercase

--- Azenqos/dprint.py
+++ Azenqos/dprint.py
@@ -8,8 +8,7 @@ debug_file_flag = os.path.isfile(
 )
 
 # print("dprint: dprint() debug_file_flag: ",debug_file_flag)
-def dprint(
-    *s,
-):  # https://stackoverflow.com/questions/919680/can-a-variable-number-of-arguments-be-passed-to-a-function
+def dprint(*s):
+    # https://stackoverflow.com/questions/919680/can-a-variable-number-of-arguments-be-passed-to-a-function
     if debug_file_flag:
         print("dprint: " + str(s))

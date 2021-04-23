28caa2849d18c5a661e6911f90508824c707089b
Kasidit Yusuf <ykasidit@gmail.com>
2021-04-23 14:43:08 +0700

    ui_test_plugin_main

--- Azenqos/ui_test_plugin_main.py
+++ Azenqos/ui_test_plugin_main.py
@@ -5,7 +5,7 @@ import azenqos_qgis_plugin
 
 def test():
     app = QApplication([])
-    azq = azenqos_qgis_plugin.azenqos_qgis_plugin(app)
+    azq = azenqos_qgis_plugin.azenqos_qgis_plugin(None)
     azq.run()
 
 

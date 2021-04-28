db7697f801b03f721931825a1e224d9569c73475
Vuttichai <vuttichai.lor@outlook.com>
2021-04-28 09:55:42 +0700

    fix log window 'time' is not defined

--- Azenqos/log_query.py
+++ Azenqos/log_query.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 def get_logs_info_df(dbcon, time_before):
-    sqlstr = "select log_hash, max(script_name) as script_name, max(script)as script, max(phonemodel) as phonemodel, max(imsi) as imsi, imei from log_info group by log_hash"
+    sqlstr = "select log_hash, time, max(script_name) as script_name, max(script)as script, max(phonemodel) as phonemodel, max(imsi) as imsi, max(imei) as imei from log_info group by log_hash"
     df = pd.read_sql(
         sqlstr,
         dbcon,
@@ -12,7 +12,7 @@ def get_logs_info_df(dbcon, time_before):
     return df
 
 def get_logs_df(dbcon, time_before):
-    sqlstr = "select log_hash, log_start_time, log_end_time, log_tag, log_ori_file_name, log_app_version, log_license_edition, log_required_pc_version, log_timezone_offset from logs group by log_hash"
+    sqlstr = "select log_hash, time, log_start_time, log_end_time, log_tag, log_ori_file_name, log_app_version, log_license_edition, log_required_pc_version, log_timezone_offset from logs group by log_hash"
     df = pd.read_sql(
         sqlstr,
         dbcon,

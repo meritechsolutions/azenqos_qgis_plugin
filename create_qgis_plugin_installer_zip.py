c59c356dc8783e038deea7b2d3dba4a1a8de79b3
Vuttichai <vuttichai.lor@outlook.com>
2021-08-02 12:42:51 +0700

    fix

--- create_qgis_plugin_installer_zip.py
+++ create_qgis_plugin_installer_zip.py
@@ -19,7 +19,7 @@ date_str = today.strftime("%Y-%m-%d")
 branch = subprocess.check_output("git branch --show-current", shell=True).decode('utf8').splitlines()[0]
 commit = subprocess.check_output("git rev-parse --short HEAD", shell=True).decode('utf8').splitlines()[0]
 target_fp = "azenqos_qgis_plugin_{}_{}_{}_{}.zip".format(date_str, branch, version.VERSION, commit)
-subprocess.check_output("git archive -o {} HEAD:Azenqos".format(target_fp), shell=True)
+subprocess.check_output("git archive -o {} HEAD ./Azenqos/*".format(target_fp), shell=True)
 print("zip gen done at:\n{}".format(target_fp))
 
 print("### push release to github")

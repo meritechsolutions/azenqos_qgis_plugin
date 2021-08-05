4f46ea282280c8b8f7c35294e205798224205ebb
Kasidit Yusuf <ykasidit@gmail.com>
2021-08-05 07:31:54 +0700

    debug new ver

--- Azenqos/azenqos_qgis_plugin.py
+++ Azenqos/azenqos_qgis_plugin.py
@@ -27,9 +27,6 @@ import sys
 import traceback
 import os
 import azq_utils
-if os.name == "nt":
-    print("windows os so calling azq_utils.open_and_redirect_stdout_to_last_run_log()")
-    azq_utils.open_and_redirect_stdout_to_last_run_log()
 from PyQt5.QtCore import QSettings, QTranslator, qVersion, QCoreApplication
 from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QAction, QMessageBox
@@ -215,7 +212,7 @@ class azenqos_qgis_plugin:
 
         # Create the dialog with elements (after translation) and keep reference
         # Only create GUI ONCE in callback, so that it will only load when the plugin is started
-
+        azq_utils.open_and_redirect_stdout_to_last_run_log()
         debug_exec_str = azq_utils.read_local_file("debug_qgis_pyexec")
         if debug_exec_str is None or debug_exec_str.strip() == "":
             pass
--- Azenqos/azq_utils.py
+++ Azenqos/azq_utils.py
@@ -1391,8 +1391,7 @@ class tee_stdout(object):
     def write(self, data):
         if self.file is not None:
             if data.strip():
-                self.file.write(time.isoformat())
-                self.write(": ")
+                self.file.write(time.strftime('%Y-%m-%d-%H:%M:%S')+": ")
                 self.file.write(data)
                 self.file.write("\n")
         self.stdout.write(data)

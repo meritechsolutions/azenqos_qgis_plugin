b65b74efd716f267f6a7ff0885848b690f1a3aa5
Suthat Thanajan <suthat@azenqos.com>
2021-08-19 10:41:10 +0700

    add all infomation when query cell_meas param for filter

--- Azenqos/db_preprocess.py
+++ Azenqos/db_preprocess.py
@@ -129,10 +129,13 @@ def prepare_spatialite_views(dbcon):
                 )
                 print("not table_has_geom so gen sql merge in from location table by time - DONE")
             else:
-                sqlstr = "create table {col} as select log_hash, time, {modem_time_part}, posid, seqid, geom, {col} from {table} ;".format(
-                    col=view, table=table,
-                    modem_time_part="modem_time" if table_has_modem_time else "null as modem_time"
-                )
+                if "cell_meas" in table:
+                    sqlstr = "create table {col} as select * from {table};".format(col=view, table=table)
+                else:
+                    sqlstr = "create table {col} as select log_hash, time, {modem_time_part}, posid, seqid, geom, {col} from {table} ;".format(
+                        col=view, table=table,
+                        modem_time_part="modem_time" if table_has_modem_time else "null as modem_time"
+                    )
             print("create view sqlstr: %s" % sqlstr)
             dbcon.execute(sqlstr)
             view_cols = pd.read_sql("select * from {} where false".format(view), dbcon).columns

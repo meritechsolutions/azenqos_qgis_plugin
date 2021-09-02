5d740156973054662d99b47167ab5bdd3eb144dd
Kasidit Yusuf <ykasidit@gmail.com>
2021-09-02 16:51:30 +0700

    dev

--- Azenqos/azm_sqlite_merge.py
+++ Azenqos/azm_sqlite_merge.py
@@ -1,4 +1,6 @@
 import sqlite3
+import subprocess
+
 import preprocess_azm
 import azq_utils
 import uuid
@@ -7,12 +9,25 @@ import contextlib
 
 
 def merge(in_azm_list, out_db_fp):
+    if os.path.isfile(out_db_fp):
+        os.remove(out_db_fp)
+    assert not os.path.exists(out_db_fp)
     tmp_dirs = []
     i = -1
     n = len(in_azm_list)
+    sqlite_bin = os.path.join(
+        azq_utils.get_module_path(),
+        os.path.join(
+            "sqlite_" + os.name,
+            "sqlite3" + ("" if os.name == "posix" else ".exe"),
+        ),
+    )
+    assert os.path.isfile(sqlite_bin)
     combine_uuid = uuid.uuid4()
+    is_last_azm = False
     with contextlib.closing(sqlite3.connect(out_db_fp)) as out_dbcon:
         for i in range(n):
+            is_last_azm = i == n-1
             tmp_dir = os.path.join(azq_utils.tmp_gen_path(), "tmp_combine_{}_azm_{}".format(combine_uuid, i))
             os.makedirs(tmp_dir)
             assert os.path.isdir(tmp_dir)
@@ -24,15 +39,18 @@ def merge(in_azm_list, out_db_fp):
             dbfp = os.path.join(tmp_dir, "azqdata.db")
             assert os.path.isfile(dbfp)
             with contextlib.closing(sqlite3.connect(dbfp)) as dbcon:
-                print("dumping db file:", dbfp)
-                sql_lines = []
-                for sql_line in dbcon.iterdump():
-                    sql_line = sql_line.replace("CREATE TABLE android_metadata (locale TEXT);", "", 1)
-                    sql_line = sql_line.replace('CREATE TABLE "', 'CREATE TABLE IF NOT EXISTS "', 1)
-                    sql_lines.append(sql_line)
-                assert sql_lines
-                sql_script = "\n".join(sql_lines)
+                cmd = [sqlite_bin, dbfp, ".dump"]
+                print("dumping db file:", dbfp, "cmd:", cmd)
+                sql_script = subprocess.check_output(cmd, encoding="utf-8")
+                sql_script = sql_script.replace('CREATE TABLE IF NOT EXISTS ', 'CREATE TABLE ')
+                sql_script = sql_script.replace('CREATE TABLE ','CREATE TABLE IF NOT EXISTS ')
+                sql_script +=" delete from spatial_ref_sys;\n"
+                if is_last_azm:
+                    sql_script += "INSERT INTO spatial_ref_sys VALUES(4326,'epsg',4326,'WGS 84','+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs');\n"
                 assert sql_script
+                with open("out.sql", "w") as f:
+                    f.write(sql_script)
+                #print("sql_script:", sql_script)
                 print("merginng to target db file:", out_db_fp)
                 out_dbcon.executescript(sql_script)
         out_dbcon.commit()
--- Azenqos/test_azm_sqlite_merge.py
+++ Azenqos/test_azm_sqlite_merge.py
@@ -5,10 +5,11 @@ import os
 def test():
     azm_list = [
         "../example_logs/nr_sa_exynos_s21_ex0/350299943614770-20_08_2021-15_55_05 (airplane then speedtest).azm",
-        "../example_logs/nr_sa_exynos_s21_ex0/350299943614770-24_08_2021-16_13_20 (speedtest then airplane ramkomut).azm"
+        "../example_logs/nr_sa_exynos_s21_ex0/350299943614770-24_08_2021-16_13_20 (speedtest then airplane ramkomut).azm",
+        "../example_logs/nr_nsa_exynos_s21/354505623113016-19_08_2021-16_45_22.azm"
     ]
-    out_azm = os.path.join(azq_utils.tmp_gen_path(), "tmp_combined.azm")
-    azm_sqlite_merge.merge(azm_list, out_azm)
+    out_db = os.path.join(azq_utils.get_module_path(), "test_combined.db")
+    azm_sqlite_merge.merge(azm_list, out_db)
     
 
 if __name__ == "__main__":

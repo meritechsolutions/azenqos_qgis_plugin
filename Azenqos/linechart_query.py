1fc7d3ee9f92677339fe51a912379ea03504631e
Vuttichai <vuttichai.lor@outlook.com>
2021-10-05 10:34:20 +0700

    Update linechart_query.py

--- Azenqos/linechart_query.py
+++ Azenqos/linechart_query.py
@@ -320,6 +320,7 @@ def get_chart_df(dbcon, param_list_dict, apply_mode=False):
         sql = "SELECT log_hash, time as Time, {} FROM {}".format(param_name, table_name)
         df = pd.read_sql(sql, dbcon, parse_dates=["Time"])
         df["log_hash"] = df["log_hash"].astype(np.int64)
+        df = df.sort_values(by="Time")
         if apply_mode:
             df["time_ms"] = df["Time"].apply(lambda x: azq_utils.datetimeStringtoTimestamp(x.strftime("%Y-%m-%d %H:%M:%S.%f")))
             df["time_ms_diff"] = df.loc[df[param_name].notna(), "time_ms"]
@@ -334,7 +335,7 @@ def get_chart_df(dbcon, param_list_dict, apply_mode=False):
             df = df.drop(columns=["null_param", "time_ms_diff", "time_ms"])
         else:
             df_merge = df.copy()
-            df_merge = df_merge.dropna()
+            df_merge = df_merge.dropna().sort_values(by="Time")
             df = pd.merge_asof(left=df.reset_index(), right=df_merge.reset_index(), left_on=['Time'], right_on=['Time'],by='log_hash',direction="backward", allow_exact_matches=True, tolerance=pd.Timedelta('2s'), suffixes=('_not_use', '')) 
             df = df[["log_hash", "Time", param_name]]
         if "data" in param_dict and param_dict["data"] == True:

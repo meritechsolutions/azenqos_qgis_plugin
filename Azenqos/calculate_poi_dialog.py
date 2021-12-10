a05b71dd261c3580f395e9c64ff1504eb5359453
Vuttichai <vuttichai.lor@outlook.com>
2021-12-10 15:42:28 +0700

    Update calculate_poi_dialog.py

--- Azenqos/calculate_poi_dialog.py
+++ Azenqos/calculate_poi_dialog.py
@@ -67,7 +67,7 @@ def get_technology_df(dbcon, cov_column_name_list):
             )
             df = pd.read_sql(sql, dbcon, parse_dates=["Time"])
             df["name"] = rat_to_main_param_dict[rat]
-            cov_column_name_list.append( rat_to_main_param_dict[rat])
+            cov_column_name_list.append(rat_to_main_param_dict[rat])
             per_rat_df_list.append(df)
         except Exception as e:
             print("WARNING: gen per_rat_df failed:", e)
@@ -75,6 +75,28 @@ def get_technology_df(dbcon, cov_column_name_list):
     df.sort_values(["log_hash", "time"], inplace=True)
     df = df.loc[(df["geom"].notna()) & (df["value"].notna())].reset_index(drop=True)
     return df ,cov_column_name_list
+
+def calculate_poi_cov(poi_list, cov_df, cov_column_name_list, lat_col, lon_col, offset):
+    row_df_list = []
+    for row in poi_list:
+        try:
+            poi_lat = float(row[lat_col])
+            poi_lon = float(row[lon_col])
+        except:
+            pass
+        if isinstance(poi_lat, float) and isinstance(poi_lon, float):
+            cov_df["poi_lat"] = poi_lat
+            cov_df["poi_lon"] = poi_lon
+            cov_df_loc = cov_df.loc[(abs(cov_df["lat"] - cov_df["poi_lat"]) <= offset) & (abs(cov_df["lon"] - cov_df["poi_lon"]) <= offset) ]
+            # cov_df["dist"] = haversine(cov_df["poi_lat"], cov_df["poi_lon"], cov_df["lat"], cov_df["lon"]) * 1000.0
+            # cov_df_loc = cov_df.loc[cov_df["dist"] <= self.offset_meters]
+            if len(cov_df_loc) > 0:
+                for cov_column_name in cov_column_name_list:
+                    avg_cov_col = cov_column_name+"_average"
+                    row[avg_cov_col] = cov_df_loc.loc[cov_df_loc["name"] == cov_column_name, "value"].mean()
+
+                row_df_list.append(row)
+    return row_df_list
 
 class calculate_poi(QDialog):
     on_result = pyqtSignal(object, object)
@@ -105,7 +127,7 @@ class calculate_poi(QDialog):
             self.ui.poiComboBox.addItem(name)
 
         self.accepted.connect(self.on_ok_button_click)
-    
+
     def on_ok_button_click(self):
         self.layer_name = self.ui.poiComboBox.currentText()
         self.radius = self.ui.radiusLineEdit.text()
@@ -113,57 +135,24 @@ class calculate_poi(QDialog):
         self.offset = azq_cell_file.METER_IN_WGS84 * self.offset_meters
         self.avg_col_list = []
         layer = QgsProject.instance().mapLayersByName(self.layer_name)[0]
-        columns = [f.name() for f in layer.fields()] + ["geometry"]
-        lon_col, lat_col = qgis_layers_gen.get_lon_lat_column_name(columns)
-        row_list = []
-        for feat in layer.getFeatures():
-            row_list.append(dict(zip(columns, feat.attributes() + [feat.geometry().asWkt()])))
-        df = pd.DataFrame(row_list, columns=columns)
-        df.columns= df.columns.str.lower()
-        df = df.drop(columns=[lon_col, lat_col])
-        df["lon"] = df["geometry"].str.extract("\((\S+)\s", expand=False).astype(float)
-        df["lat"] = df["geometry"].str.extract("\s(\S+)\)", expand=False).astype(float)
-        df = df.drop(columns=["geometry"])
-        df = df.dropna().reset_index(drop=True)
-        df["radius"] = None
-        self.df_columns = df.columns.tolist()
-        cov_df_list = []
+        columns = [f.name() for f in layer.fields()]
+        self.lon_col, self.lat_col = qgis_layers_gen.get_lon_lat_column_name(columns)
+        columns = [column.lower() for column in columns]
+        poi_list = []
+        for feat in layer.getFeatures(QgsFeatureRequest().setFlags(QgsFeatureRequest.NoGeometry)):
+            poi_list.append(dict(zip(columns, feat.attributes())))
+        df = pd.DataFrame()
         cov_column_name_list = []
-        
+        print(self.offset)
+        row_df_list = []
         if self.gc.databasePath is not None:
             with contextlib.closing(sqlite3.connect(self.gc.databasePath)) as dbcon:
                 cov_df, cov_column_name_list = get_technology_df(dbcon, cov_column_name_list)
                 cov_df["lat"] = cov_df["geom"].apply(lambda x: geomToLatLon(x)[0])
                 cov_df["lon"] = cov_df["geom"].apply(lambda x: geomToLatLon(x)[1])
-                if len(cov_df) > 0:
-                    if self.apply_mode == True:
-                        def filter_cov(row, cov_df, cov_column_name_list):
-                            if row["lat"] is not None and row["lon"] is not None :
-                                cov_df["poi_lat"] = row["lat"]
-                                cov_df["poi_lon"] = row["lon"]
-                                cov_df["dist"] = haversine(cov_df["poi_lat"], cov_df["poi_lon"], cov_df["lat"], cov_df["lon"]) * 1000.0
-                                cov_df = cov_df.loc[cov_df["dist"] <= self.offset_meters]
-                                for cov_column_name in cov_column_name_list:
-                                    if len(cov_df) > 0:
-                                        avg_cov_col = cov_column_name+"_average"
-                                        if avg_cov_col not in self.avg_col_list:
-                                            self.avg_col_list.append(avg_cov_col)
-                                        row[avg_cov_col] = cov_df.loc[cov_df["name"] == cov_column_name, "value"].mean()
-                                        row["radius"] = str(self.offset_meters / 1000.0) +"km."
-                            return row
-                        df = df.apply(lambda row: filter_cov(row, cov_df, cov_column_name_list), axis=1)
-                        df = df[self.df_columns+self.avg_col_list]
-                    else:
-                        poi_df = df.reset_index()
-                        cov_lon_df = cov_df.sort_values(by="lon").reset_index(drop=True)
-                        poi_lon_df = poi_df.sort_values(by="lon").reset_index(drop=True)
-                        cov_lat_df = cov_df.sort_values(by="lat").reset_index(drop=True)
-                        poi_lat_df = poi_df.sort_values(by="lat").reset_index(drop=True)
-                        df1 = pd.merge_asof(left=cov_lon_df, right=poi_lon_df, left_on=["lon"], right_on=["lon"] ,direction="nearest", allow_exact_matches=True, tolerance=self.offset, suffixes=("", "_not_use"))
-                        df2 = pd.merge_asof(left=cov_lat_df, right=poi_lat_df, left_on=["lat"], right_on=["lat"] ,direction="nearest", allow_exact_matches=True, tolerance=self.offset, suffixes=("", "_not_use"))
-                        df = df1.merge(df2, how="left", on="index", suffixes=("", "_not_use"))
-                        print("df", df)
+                if len(poi_list) > 0:
+                   row_df_list = self.calculate_poi_cov(poi_list, cov_df, cov_column_name_list, self.lat_col, self.lon_col, self.offset )
+                df = pd.DataFrame(row_df_list)
 
-        df = df.loc[df["radius"].notna()].reset_index(drop=True)
-        window_name = "Coverage" + self.radius + "m. around poi: " + self.layer_name
+        window_name = "Coverage " + str(self.offset_meters / 1000.0) + "km. around poi: " + self.layer_name
         self.on_result.emit(df, window_name)

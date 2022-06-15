919039fff0bff6f38597c634e52fb6b5f3c067fb
Vuttichai <vuttichai.lor@outlook.com>
2022-06-15 09:37:22 +0700

    fix install requirement

--- Azenqos/check_and_install_requirements.py
+++ Azenqos/check_and_install_requirements.py
@@ -1,5 +1,7 @@
 import subprocess
 import os
+import sys
+
 import qt_utils
 
 def get_module_path():
@@ -13,6 +15,15 @@ def check_and_install_requirements():
     needs_install = False
 
     # pkg_list = [x.strip() for x in pkg_list]
+    wheel_dp = get_local_fp('wheel')
+    assert os.path.isdir(wheel_dp)
+    cp_pattern = f"cp{sys.version_info.major}{sys.version_info.minor}"
+    compat_wheel_pattern = f"*-*-{cp_pattern}*.whl"
+    
+    import glob
+    whl_list = glob.glob(os.path.join(wheel_dp, compat_wheel_pattern))
+    whl_list = sorted(whl_list) #install numpy before scipy
+
     requirement_fp = get_local_fp('requirements.txt')
     requirement_list = None
     with open(requirement_fp,"r") as f:
@@ -34,10 +45,10 @@ def check_and_install_requirements():
 
     if needs_install:
         print("need_to_restart")
-        cmd = ['python', '-m', 'pip', 'install', '-r', requirement_fp]
-        print("cmd:", cmd)
+        for whl in whl_list:
+            subprocess.call(['python', '-m', 'pip', 'install', whl])
         try:
-            subprocess.check_output(['python', '-m', 'pip', 'install', '-r', requirement_fp],stderr=subprocess.STDOUT)
+            subprocess.check_output(['python', '-m', 'pip', 'install', '-r', requirement_fp], stderr=subprocess.STDOUT)
         except subprocess.CalledProcessError as e:
             outstr = e.output
             qt_utils.msgbox("Azenqos plugin failed to install required dependencies, please email below error msg to support@azenqos.com:\n\n" + outstr.decode("utf-8"), title="AZENQOS Dependency Install Fail")

14e8eb21cdc1a6147c820ee62a11fe14861db066
Vuttichai <vuttichai.lor@outlook.com>
2022-07-06 11:59:28 +0700

    Update test_preferences_ini.py

--- Azenqos/test_preferences_ini.py
+++ Azenqos/test_preferences_ini.py
@@ -12,9 +12,9 @@ def test():
     vv.pref["spider_match_max_distance_meters"] = "500"
     vv.save_preferences()
     assert True == os.path.isfile(azq_utils.get_settings_fp("preferences.ini"))
-    assert "15000" == analyzer_vars.analyzer_vars().pref["spider_match_max_distance_meters"]
+    assert "500" == analyzer_vars.analyzer_vars().pref["spider_match_max_distance_meters"]
     vv.load_preferences()
-    assert "15000" == analyzer_vars.analyzer_vars().pref["spider_match_max_distance_meters"]
+    assert "500" == analyzer_vars.analyzer_vars().pref["spider_match_max_distance_meters"]
     vv.delete_preferences()
     assert False == os.path.isfile(azq_utils.get_settings_fp("preferences.ini"))
     vv.load_preferences()
